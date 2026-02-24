96881f8b52cdf6629837ff6e601c151fa625e5c7
rajveer jaddhav <jadhavrajveer86@gmail.com>
2026-02-24 23:32:36 +0530

    a

--- takeinputofVELOCITY.py
+++
@@ -1,8 +0,0 @@
-s = float(input("Enter specific gravity of particle: "))
-s1 = float(input("Enter specific gravity of water: "))
-d = float(input("Enter diameter (mm): "))
-T = float(input("Enter temperature (°C): "))
-
-v = 418 * (s - s1) * (d ** 2) *  (3 * T + 70) / 100
-
-print("Velocity of settlement =", round(v, 3), "mm/sec")
